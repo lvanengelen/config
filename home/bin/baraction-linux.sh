52c2196edd30d690b85703ad1f5651bb24bb249d
Leroy van Engelen <leroy.vanengelen@gmail.com>
2013-09-07 18:53:20 +0200

    Replace tabs with spaces and move to 4 space indents

--- home/bin/baraction-linux.sh
+++ home/bin/baraction-linux.sh
@@ -3,26 +3,26 @@
 SLEEP_SEC=5
 
 while :; do
-        TEMP_STR=$(sensors -u | \
-            awk '/temp.*_input/ { printf "%d ", $2 }' | \
-            awk '{ printf "Temps:%d,%d,%d,%d,%ddegC", $1, $2, $3, $4, $5 }')
+    TEMP_STR=$(sensors -u | \
+        awk '/temp.*_input/ { printf "%d ", $2 }' | \
+        awk '{ printf "Temps:%d,%d,%d,%d,%ddegC", $1, $2, $3, $4, $5 }')
 
-	eval $(cat /proc/net/wireless | sed s/[.]//g | awk '/^[[:alnum:]]+:/ {printf "WLAN_QULTY=%s; WLAN_SIGNL=%s; WLAN_NOISE=%s", $3,$4,$5};' -)
-	BCSCRIPT="scale=0;a=100*$WLAN_QULTY/70;print a"
-	WLAN_QPCT=`echo $BCSCRIPT | bc -l`
-	WLAN_STR="Q=$WLAN_QPCT% S/N="$WLAN_SIGNL"/"$WLAN_NOISE"dBm"
+    eval $(cat /proc/net/wireless | sed s/[.]//g | awk '/^[[:alnum:]]+:/ {printf "WLAN_QULTY=%s; WLAN_SIGNL=%s; WLAN_NOISE=%s", $3,$4,$5};' -)
+    BCSCRIPT="scale=0;a=100*$WLAN_QULTY/70;print a"
+    WLAN_QPCT=`echo $BCSCRIPT | bc -l`
+    WLAN_STR="Q=$WLAN_QPCT% S/N="$WLAN_SIGNL"/"$WLAN_NOISE"dBm"
 
-	CPUFREQ_STR=`echo "Freq:"$(cat /proc/cpuinfo | grep 'cpu MHz' | sed 's/.*: //g; s/\..*//g;')`
-	CPULOAD_STR="Load:$(uptime | sed 's/.*://; s/,//g')"
+    CPUFREQ_STR=`echo "Freq:"$(cat /proc/cpuinfo | grep 'cpu MHz' | sed 's/.*: //g; s/\..*//g;')`
+    CPULOAD_STR="Load:$(uptime | sed 's/.*://; s/,//g')"
 
-	eval $(awk '/^MemTotal/ {printf "MTOT=%s;", $2}; /^MemFree/ {printf "MFREE=%s;",$2}' /proc/meminfo)
-	MUSED=$(( $MTOT - $MFREE ))
-	MUSEDPT=$(( ($MUSED * 100) / $MTOT ))
-	MEM_STR="Mem:${MUSEDPT}%"
+    eval $(awk '/^MemTotal/ {printf "MTOT=%s;", $2}; /^MemFree/ {printf "MFREE=%s;",$2}' /proc/meminfo)
+    MUSED=$(( $MTOT - $MFREE ))
+    MUSEDPT=$(( ($MUSED * 100) / $MTOT ))
+    MEM_STR="Mem:${MUSEDPT}%"
 
-        MIXER_STR="Vol:$(amixer get Master | sed -n '$s/^.*\[\(.*\)%].*$/\1/p')%"
+    MIXER_STR="Vol:$(amixer get Master | sed -n '$s/^.*\[\(.*\)%].*$/\1/p')%"
 
-	echo -e "$TEMP_STR  $MEM_STR  $WLAN_STR  $MIXER_STR"
+    echo -e "$TEMP_STR  $MEM_STR  $WLAN_STR  $MIXER_STR"
 
-	sleep $SLEEP_SEC
+    sleep $SLEEP_SEC
 done
