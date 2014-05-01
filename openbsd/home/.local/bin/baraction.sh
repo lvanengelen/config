ab23234bb0d68d559fed7e989baac1430937b0bf
Leroy van Engelen <leroy.vanengelen@gmail.com>
2014-05-01 22:58:23 +0200

    Refactor to make data retrieval a bit clearer.

--- openbsd/home/.local/bin/baraction.sh
+++ openbsd/home/.local/bin/baraction.sh
@@ -2,16 +2,19 @@
 #
 # $scrotwm: baraction.sh,v 1.17 2010/07/01 19:49:37 marco Exp $
 
-print_mem() {
+update_mem() {
     MEM=`/usr/bin/top | grep Free: | cut -d " " -f6`
-    echo -n "M: $MEM "
 }
 
-_print_cpu() {
-    echo -n "U: ${1}% N: ${2}% S: ${3}% I: ${4}% Id: ${5}% "
+_update_cpu() {
+    CPU_USR=$1
+    CPU_NCE=$2
+    CPU_SYS=$3
+    CPU_INT=$4
+    CPU_IDL=$5
 }
 
-print_cpu() {
+update_cpu() {
     OUT=""
     # iostat prints each column justified to 3 chars, so if one counter
     # is 100, it jams up agains the preceeding one. sort this out.
@@ -23,68 +26,74 @@ print_cpu() {
         fi
         shift;
     done
-    _print_cpu $OUT
+    _update_cpu $OUT
+
+    CPU_SPEED=`/sbin/sysctl hw.cpuspeed | cut -d "=" -f2`
 }
 
-print_apm() {
+_update_apm() {
     BAT_STATUS=$1
     BAT_LEVEL=$2
     AC_STATUS=$3
-
-    if [ $AC_STATUS -ne 255 -o $BAT_STATUS -lt 4 ]; then
-        if [ $AC_STATUS -eq 0 ]; then
-            echo -n "on batt (${BAT_LEVEL}%)"
-        else
-            case $AC_STATUS in
-            1)
-                AC_STRING="AC: "
-                ;;
-            2)
-                AC_STRING="backup AC: "
-                ;;
-            *)
-                AC_STRING=""
-                ;;
-            esac;
-            case $BAT_STATUS in
-            4)
-                BAT_STRING="(no batt)"
-                ;;
-            [0-3])
-                BAT_STRING="(batt ${BAT_LEVEL}%)"
-                ;;
-            *)
-                BAT_STRING="(batt ???)"
-                ;;
-            esac;
-        
-            FULL="${AC_STRING}${BAT_STRING} "
-            if [ "$FULL" != "" ]; then
-                echo -n "$FULL"
-            fi
-        fi
-    fi
-    echo -n "  "
 }
 
-print_cpuspeed() {
-    CPU_SPEED=`/sbin/sysctl hw.cpuspeed | cut -d "=" -f2`
-    echo -n "$CPU_SPEED MHz "
+update_apm() {
+    _update_apm `/usr/sbin/apm -alb`
 }
 
-print_wifi_signal() {
-    WIFI_SIGNAL=`/sbin/ifconfig athn0 | grep ieee80211 | cut -d' ' -f 8`
-    echo -n "W: $WIFI_SIGNAL  "
+update_ifs() {
+    NETIFS=`systat ifstat | grep up:U`
 }
 
-print_volume() {
+update_wifi_signal() {
+    WIFI_SIGNAL=`/sbin/ifconfig $1 | grep ieee80211 | cut -d' ' -f 8`
+}
+
+update_volume() {
     MUTE=`mixerctl outputs.master.mute | cut -d= -f 2`
-    if [ "x$MUTE" = "xon" ]; then
-        VOLUME=mute
-    else
-        VOLUME=`mixerctl -n outputs.master`
+    VOLUME=`mixerctl -n outputs.master`
+}
+
+print_mem() {
+    echo -n "$MEM"
+}
+
+print_cpu() {
+    echo -n "$CPU_USR $CPU_NCE $CPU_SYS $CPU_INT $CPU_IDL $CPU_SPEED"
+}
+
+print_bat() {
+    case $AC_STATUS in
+        1)
+            AC_STRING="AC "
+            ;;
+        2)
+            AC_STRING="ac "
+            ;;
+        *)
+            AC_STRING=""
+            ;;
+    esac
+
+    if [ $BAT_STATUS -lt 4 ]; then
+        echo -n "$AC_STRING$BAT_LEVEL%"
     fi
-    echo -n "V: $VOLUME "
+}
+
+print_ifs() {
+    echo $NETIFS | while read nif; do
+        update_wifi_signal
+        echo -n "$WIFI_SIGNAL$x"
+        x=" "
+    done
+}
+
+print_vol() {
+    if [ $MUTE = "on" ]; then
+        echo -n "mute"
+    else
+        echo -n "$VOLUME"
+    fi
 }
 
 while :; do
@@ -92,23 +101,32 @@ while :; do
     # cache the output of apm(8), no need to call that every second.
     /usr/sbin/iostat -C -c 3600 |&  # wish infinity was an option
     PID="$!"
-    APM_DATA=""
     I=0
     trap "kill $PID; exit" TERM
+    
     while read -p; do
-        if [ $(( ${I} % 1 )) -eq 0 ]; then
-            APM_DATA=`/usr/sbin/apm -alb`
+        if [ $(( ${I} % 5 )) -eq 0 ]; then
+            update_apm
+            update_ifs
         fi
+
         if [ $I -gt 2 ]; then
-            # print_date
-            print_mem $MEM
-            print_cpu $REPLY
-            print_cpuspeed
-            print_apm $APM_DATA
-            print_wifi_signal
-            print_volume
+            update_mem
+            update_cpu $REPLY
+            update_volume
+
+            print_mem
+            echo -n " "
+            print_cpu
+            echo -n " "
+            print_bat
+            echo -n " "
+            print_ifs
+            echo -n " "
+            print_vol
             echo ""
         fi
+
         I=$(( ${I} + 1 ));
     done
 done
